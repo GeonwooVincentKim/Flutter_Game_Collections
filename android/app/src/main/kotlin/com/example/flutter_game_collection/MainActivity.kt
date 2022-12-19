925d464d9beb4a435f5014324f241032b4246f36
GeonwooVincentKim <kdsnop@gmail.com>
2022-12-19 14:48:58 +0900

    [Check] - Checking..

---
+++ android/app/src/main/kotlin/com/example/flutter_game_collection/MainActivity.kt
@@ -0,0 +1,7 @@
+// package com.example.flutter_exercises_example
+package com.example.flutter_game_collection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
